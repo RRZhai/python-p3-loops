4bcb998ddfded9c15ef52f3264239f78cfb51d00
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-05-23 17:30:39 -0700

    passed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+count = 10
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(f'{count}')
+        count -= 1
+    print('Happy New Year!')
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = []
+    for item in int_list:
+        square_int = item*item
+        square_list.append(square_int)
+    return square_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while count <= 100:
+        if count%3 == 0 and count%5==0:
+            print("FizzBuzz")
+        elif count%5 == 0:
+            print('Buzz')
+        elif count%3 == 0:
+            print('Fizz')
+        else: 
+            print(f'{count}')
+        count +=1
